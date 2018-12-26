7be322bee14e300bd7c14ff76481148e2ea6bebc
derlinchen <derlin_nj@163.com>
2018-12-26 20:57:56 +0800

    序列化处理

---
+++ src/main/java/com/climber/utils/SerializeUtil.java
@@ -0,0 +1,79 @@
+package com.climber.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+/**
+ * 对象进行序列化的类
+ * @author Derlin
+ *
+ */
+public class SerializeUtil {
+
+	/**
+	 * 将对象进行序列化
+	 * @param obj
+	 * @return
+	 */
+	public static byte[] serialize(Object obj) {
+		ObjectOutputStream oos = null;
+		ByteArrayOutputStream baos = null;
+		try {
+			baos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(obj);
+			byte[] byt = baos.toByteArray();
+			return byt;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (baos != null) {
+					baos.close();
+				}
+				if (oos != null) {
+					oos.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 将byte[]反序列化
+	 * @param byt
+	 * @return
+	 */
+	public static Object unserizlize(byte[] byt) {
+		ObjectInputStream ois = null;
+		ByteArrayInputStream bais = null;
+		bais = new ByteArrayInputStream(byt);
+		try {
+			ois = new ObjectInputStream(bais);
+			Object obj = ois.readObject();
+			return obj;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ois != null) {
+					ois.close();
+				}
+				
+				if (bais != null) {
+					bais.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+}
